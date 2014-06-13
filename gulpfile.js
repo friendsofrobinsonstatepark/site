6b0cd75d10f1784c344d70fce61b29587c3837e4
Ryan Muller <ryan@muller.io>
2014-06-13 16:04:56 -0400

    Home page is nearly done

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var connect = require('connect');
+var sass = require('gulp-sass');
+
+gulp.task('default', ['render-jade','styles', 'backgrounds','icons'], function(){
+});
+
+gulp.task('render-jade', function(){
+  gulp.src('./src/*.jade')
+                   .pipe(jade())
+                   .pipe(gulp.dest('./build'));
+});
+
+gulp.task('styles', function(){
+  gulp.src('./sass/*.sass')
+      .pipe(sass())
+      .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('icons', function(){
+  gulp.src('./icons/*')
+                   .pipe(gulp.dest('./build'));
+});
+
+gulp.task('backgrounds', function(){
+  gulp.src('./backgrounds/*')
+                   .pipe(gulp.dest('./build'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./src/*.jade', ['render-jade']);
+  gulp.watch('./sass/*.sass', ['styles']);
+  connect().use(connect.static(__dirname+'/build')).listen(8080);
+  console.log('Site available at localhost:8080');
+});
